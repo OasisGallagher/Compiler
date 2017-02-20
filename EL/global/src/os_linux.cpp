1290e4804e8a9ad64c360182d45a7d47687422cc
wangluo <rainpatrol@live.com>
2017-02-20 14:07:22 +0800

    无用的函数.

--- EL/global/src/os_linux.cpp
+++ EL/global/src/os_linux.cpp
@@ -15,9 +15,6 @@ time_t OS::GetFileLastWriteTime(const char* fileName) {
 	return 0;
 }
 
-void OS::CopyFile(const char* oldFileName, const char* newFileName) {
-}
-
 bool OS::Prompt(const char* message) {
 	return true;
 }
